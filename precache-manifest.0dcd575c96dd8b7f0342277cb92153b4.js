3fecd01a8299baba97531b56aab038928473c53f
Kishandixit <kishandixit2000@gmail.com>
2022-12-04 15:10:17 +0700

    Updates

---
+++ precache-manifest.0dcd575c96dd8b7f0342277cb92153b4.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = [
+  {
+    "revision": "546a24e483cdf80b4315",
+    "url": "/kishan-portfolio-website/static/css/main.8759da46.chunk.css"
+  },
+  {
+    "revision": "546a24e483cdf80b4315",
+    "url": "/kishan-portfolio-website/static/js/main.546a24e4.chunk.js"
+  },
+  {
+    "revision": "d38164de74c419fde16c",
+    "url": "/kishan-portfolio-website/static/css/1.992b143b.chunk.css"
+  },
+  {
+    "revision": "d38164de74c419fde16c",
+    "url": "/kishan-portfolio-website/static/js/1.d38164de.chunk.js"
+  },
+  {
+    "revision": "ca4aacd3f03e7dcb8e32",
+    "url": "/kishan-portfolio-website/static/js/runtime~main.ca4aacd3.js"
+  },
+  {
+    "revision": "00198e3e60f87f83a3379eb4fb502c9b",
+    "url": "/kishan-portfolio-website/static/media/Kishan_Resume.00198e3e.pdf"
+  },
+  {
+    "revision": "4afc75bdc3e3dd2c73f67232f4220e29",
+    "url": "/kishan-portfolio-website/static/media/me.4afc75bd.png"
+  },
+  {
+    "revision": "731ea47c79ab93c840a8ded9a5d2d7a8",
+    "url": "/kishan-portfolio-website/static/media/me_about.731ea47c.png"
+  },
+  {
+    "revision": "1ac317076a81a46e28f6df70e5e83d64",
+    "url": "/kishan-portfolio-website/static/media/Project-Img1.1ac31707.png"
+  },
+  {
+    "revision": "3b8c15a4ec41f744ab7e37293e720106",
+    "url": "/kishan-portfolio-website/static/media/Project-Img2.3b8c15a4.jpg"
+  },
+  {
+    "revision": "f90e8734ff044a6b0732fceb79ed2966",
+    "url": "/kishan-portfolio-website/static/media/Project-Img3.f90e8734.jpg"
+  },
+  {
+    "revision": "b4e6083009d003fdbf5b1522015fdd5c",
+    "url": "/kishan-portfolio-website/static/media/acheive1.b4e60830.jpg"
+  },
+  {
+    "revision": "4223de190c25eaa807328e472a76d6f4",
+    "url": "/kishan-portfolio-website/static/media/acheive2.4223de19.jpeg"
+  },
+  {
+    "revision": "807ce87d48b41dc743bef4921e0f62aa",
+    "url": "/kishan-portfolio-website/static/media/acheive3.807ce87d.jpeg"
+  },
+  {
+    "revision": "2fa53c9bc56a7ad794c23bbe540d0872",
+    "url": "/kishan-portfolio-website/static/media/acheive4.2fa53c9b.jpeg"
+  },
+  {
+    "revision": "dd020346f70bd04c192f9d9d68ce2461",
+    "url": "/kishan-portfolio-website/static/media/acheive5.dd020346.jpeg"
+  },
+  {
+    "revision": "b8997981f30f892723b144d13051d259",
+    "url": "/kishan-portfolio-website/static/media/acheive6.b8997981.jpeg"
+  },
+  {
+    "revision": "9f127760ff3e50e4c734333836d67f82",
+    "url": "/kishan-portfolio-website/static/media/acheive7.9f127760.jpeg"
+  },
+  {
+    "revision": "07d18482d4c4cb3b517ce53e1bab3738",
+    "url": "/kishan-portfolio-website/static/media/acheive8.07d18482.jpg"
+  },
+  {
+    "revision": "8fbac88083b3f5dea94964730fbadecd",
+    "url": "/kishan-portfolio-website/static/media/acheive9.8fbac880.jpg"
+  },
+  {
+    "revision": "fe903d795f94b17a36ab7dbf12e25806",
+    "url": "/kishan-portfolio-website/static/media/acheive10.fe903d79.jpg"
+  },
+  {
+    "revision": "7c6c0c0eb4af4c651302d5fc0c0a5339",
+    "url": "/kishan-portfolio-website/static/media/acheive11.7c6c0c0e.jpg"
+  },
+  {
+    "revision": "4c5c6afb4bcbcb975a677c9d706f5b15",
+    "url": "/kishan-portfolio-website/static/media/acheive12.4c5c6afb.jpg"
+  },
+  {
+    "revision": "6733a07df753db9f8a13f291239eecc1",
+    "url": "/kishan-portfolio-website/static/media/acheive13.6733a07d.jpg"
+  },
+  {
+    "revision": "87348675c70aa69e4660ebbc7e0d6d4f",
+    "url": "/kishan-portfolio-website/static/media/acheive14.87348675.jpg"
+  },
+  {
+    "revision": "b73335511e215a46c4aa8906fb23d16d",
+    "url": "/kishan-portfolio-website/index.html"
+  }
+];
